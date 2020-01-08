fd497a947da2cd949e8bb22e6c69664a7367759e
Tim Woodland <tim.woodland@clearesult.com>
2020-01-07 18:04:37 -0800

    Updates to support some app_settings and additional resource options

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ resource "azurerm_function_app" "functionapp" {
   resource_group_name       = var.rg_name
   app_service_plan_id       = local.plan
   storage_connection_string = var.storage_connection_string
+  enable_builtin_logging    = var.enable_builtin_logging
+  version                   = var.function_version
+
+  app_settings = {
+    FUNCTIONS_WORKER_RUNTIME = var.functions_worker_runtime
+    WEBSITE_RUN_FROM_PACKAGE = var.website_run_from_package
+  }
 
   lifecycle {
     ignore_changes = [
--- variables.tf
+++ variables.tf
@@ -64,6 +64,26 @@ variable "storage_connection_string" {
   description = "."
 }
 
+variable "function_version" {
+  default = "~2"
+  description = "Runtime version of Azure Function App.  Values are '~1', '~2', '~3'."
+}
+
+variable "functions_worker_runtime" {
+  default = "dotnet"
+  description = "The language worker runtime to load into the function app.  Valid values are 'dotnet','node','java','powershell', 'python'.  The default value is 'dotnet'."
+}
+
+variable "website_run_from_package" {
+ default = "0"
+ description = "This enables your function app to run from a mounted package file.  See documentation for instructions"
+}
+
+variable "enable_builtin_logging" {
+  default = "false"
+  description = "This determines whether to enable builtin logging.  The default is false, under the premis that the function logs to Application Insights."
+}
+
 variable "tags" {
   type        = map(string)
   default     = {}
@@ -73,13 +93,8 @@ variable "tags" {
 # Compute default name values
 locals {
   plan_name = var.plan != "" ? split("/", var.plan)[8] : var.plan_name
-  plan_rg = var.plan != "" ? split("/", var.plan)[4] : var.plan_rg
-  plan = var.plan != "" ? var.plan : format("/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Web/serverFarms/%s", data.azurerm_client_config.current.subscription_id, var.plan_rg, var.plan_name)
-
-  #storage_account_name           = var.storage_account_id != "" ? split("/", var.storage_account_id)[8] : var.storage_account_name
-  #storage_account_resource_group = var.storage_account_id != "" ? split("/", var.storage_account_id)[4] : var.storage_account_resource_group
-  #storage_account                = var.storage_account_id != "" ? var.storage_account_id : format("/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Storage/storageAccounts/%s", data.azurerm_client_config.current.subscription_id, var.storage_account_resource_group, var.storage_account_name)
-
+  plan_rg   = var.plan != "" ? split("/", var.plan)[4] : var.plan_rg
+  plan      = var.plan != "" ? var.plan : format("/subscriptions/%s/resourceGroups/%s/providers/Microsoft.Web/serverFarms/%s", data.azurerm_client_config.current.subscription_id, var.plan_rg, var.plan_name)
 
   env_id = lookup(module.naming.env-map, var.environment, "env")
   type   = lookup(module.naming.type-map, "azurerm_function_app", "typ")
@@ -91,8 +106,6 @@ locals {
 
   name_prefix = var.name_prefix != "" ? var.name_prefix : local.default_name_prefix
   name        = format("%s%s", local.name_prefix, local.type)
-
-  #storage_connection_string = data.azurerm_storage_account.storageaccount.primary_connection_string
 
 }
 
